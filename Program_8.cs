73be73d39ae81d02f6185f05658d7a6ec31b46b8
NGreben <96392178+NGreben@users.noreply.github.com>
2023-03-12 13:24:59 +0300

    Add files via upload

---
+++ Program_8.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int N;
+int number = 1;
+
+Console.Write($"Введите число N: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+while (number != N + 1)
+{
+    if (number % 2 == 0)
+        Console.Write($"{number} ");
+    number = number + 1;
+}
